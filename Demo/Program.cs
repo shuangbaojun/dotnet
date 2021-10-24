aaeb7dd94c728a12f790ec44fee39c487ba2b2a6
shuangbaojun <190663855@qq.com>
2021-10-24 12:44:29 +0800

    add thread name

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -8,7 +8,7 @@ namespace Demo
         static void Main(string[] args)
         {
             Thread t = new Thread(WriteY);  // 开辟了一个新的线程 Thread
-            t.Name = "Y Thread ...";
+            //t.Name = "Y Thread ...";
             t.Start();  //运行 WriteY();
 
             // 同时在主线程也在做一些工作
