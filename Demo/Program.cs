f8a89a4329fa3ae97ad6933d020082d6807139b0
shuangbaojun <190663855@qq.com>
2021-10-24 12:37:43 +0800

    thread-1

---
+++ Demo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading;
+using System;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(WriteY);  // 开辟了一个新的线程 Thread
+            t.Name = "Y Thread ...";
+            t.Start();  //运行 WriteY();
+
+            // 同时在主线程也在做一些工作
+            for (var i = 0; i < 1000; i++)
+            {
+                Console.Write("x");
+            }
+        }
+
+        static void WriteY()
+        {
+            for (var i = 0; i < 1000; i++)
+            {
+                Console.Write("y");
+            }
+        }
+    }
+
+}
